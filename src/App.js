72c3f6260b7eddc34c95b00574ff6eaeac6fd7c7
aldngrha <aldi.nugrahatk@gmail.com>
2021-04-02 15:48:18 +0700

    adding components to LandingPage

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import LandingPages from "pages/LandingPages";
+import DetailsPage from "pages/DetailPage";
 import "assets/scss/style.scss";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Route path="/" component={LandingPages}></Route>
+        <Route exact path="/" component={LandingPages}></Route>
+        <Route exact path="/properties/:id" component={DetailsPage}></Route>
       </Router>
     </div>
   );
