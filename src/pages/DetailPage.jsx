0186cf7d7585700b5696a467a936c0427b7d8a52
aldngrha <aldi.nugrahatk@gmail.com>
2021-04-05 12:34:28 +0700

    adding components to Detail Page

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
 import Header from "components/Header";
 import PagetDetailTitle from "components/PageDetailTitle";
-import ItemDetails from "json/itemDetails.json";
 import FeaturedImage from "components/FeaturedImage";
+import PageDetailDescription from "components/PageDetailDescription";
+import BookingForm from "components/BookingForm";
+import Categories from "components/Categories";
+import Testimonial from "components/Testimonial";
+import Footer from "components/Footer";
+
+import ItemDetails from "json/itemDetails.json";
 
 export default class DetailPage extends Component {
   componentDidMount() {
@@ -17,12 +24,26 @@ export default class DetailPage extends Component {
     ];
     return (
       <>
-        <Header {...this.props}></Header>
-        <PagetDetailTitle
-          breadcrumb={breadcrumb}
-          data={ItemDetails}
-        ></PagetDetailTitle>
-        <FeaturedImage data={ItemDetails.imageUrls}></FeaturedImage>
+        <Header {...this.props} />
+        <PagetDetailTitle breadcrumb={breadcrumb} data={ItemDetails} />
+        <FeaturedImage data={ItemDetails.imageUrls} />
+        <section className="container">
+          <div className="row">
+            <div className="col-7 pr-5">
+              <Fade bottom>
+                <PageDetailDescription data={ItemDetails} />
+              </Fade>
+            </div>
+            <div className="col-5">
+              <Fade bottom>
+                <BookingForm itemDetails={ItemDetails} />
+              </Fade>
+            </div>
+          </div>
+        </section>
+        <Categories data={ItemDetails.categories} />
+        <Testimonial data={ItemDetails.testimonial} />
+        <Footer />
       </>
     );
   }
